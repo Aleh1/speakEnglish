266d061bafd7bd392cfa19c7c20213773fa98324
Aleh1 <krytiak3@gmail.com>
2023-12-05 22:42:47 +0300

    добавил больше слов в базу данных, подключил озвучивание текста

--- speakEnglish.py
+++ speakEnglish.py
@@ -1,6 +1,7 @@
 #version 1 
 
 import mysql.connector
+import pyttsx3 
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -15,6 +16,16 @@ username = input("Введите номер от 1 до 2: ")
 input_namber = "select  * from words where id =" + username 
 mycursor.execute(input_namber)
 myresult = mycursor.fetchall()
+# вытягиваем слово из списка
 word = ((myresult[0])[1])
 # выводим результат
 print(word)
+
+# включаем озвучивание текста
+converter = pyttsx3.init() 
+# speed 
+converter.setProperty('rate', 170) 
+# Set volume 0-1 громкость
+converter.setProperty('volume', 1) 
+converter.say(word)   
+converter.runAndWait() 
