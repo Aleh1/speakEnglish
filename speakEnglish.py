0539273bcd704c01610a9e5588476abc443902ae
Aleh1 <krytiak3@gmail.com>
2023-12-08 19:59:47 +0300

    добавил программу номер 3 , подключил модуль random

--- speakEnglish.py
+++ speakEnglish.py
@@ -2,6 +2,7 @@
 
 import mysql.connector
 import pyttsx3 
+import random
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -13,10 +14,10 @@ mydb = mysql.connector.connect(
 while True:
     print('1 программа изучения слов')
     print('2 программа озвучивания текста')
+    print('3 программа рандомный выбор слов')
 
     command = input('Выберите пункт: ')
     if command == '1':
-
         while True:
             mycursor = mydb.cursor()
             # ввод в ручном режиме
@@ -50,5 +51,32 @@ while True:
         converter.say(speak)   
         converter.runAndWait() 
         break
+
+       
+    elif command == '3':
+        while True:
+            mycursor = mydb.cursor()
+            # ввод в рандомном режиме
+            value = random.randint(1,10)
+            #print(value)
+            input_namber = "select * from words where id =" + str(value) 
+            mycursor.execute(input_namber)
+            myresult = mycursor.fetchall()
+            # вытягиваем слово из списка
+            word = ((myresult[0])[1])
+            # выводим результат
+            print(word)
+
+            # включаем озвучивание текста
+            converter = pyttsx3.init() 
+            # speed 
+            converter.setProperty('rate', 110) 
+            # Set volume 0-1 громкость
+            converter.setProperty('volume', 1) 
+            converter.say(word)   
+            converter.runAndWait() 
+            #print('Введите Enter')
+            ent = input ("Для продолжения нажмите Enter")
+
     else:
         print('Вы ввели не правильное значение')
